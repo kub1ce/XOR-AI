d0adc74ef4640fd1100a995fba741101f57eb6cb
Kubice <24kub_yt@mail.ru>
2025-04-10 10:46:28 +0500

    small fix

--- app/handlers/greeting.py
+++ app/handlers/greeting.py
@@ -5,5 +5,5 @@ greetingRouter = Router(name="Start")
 
 @greetingRouter.message(filters.CommandStart())
 async def greeting(msg: Message):
-        await msg.answer(msg.from_user.id, "Crocodillo Bombordiro")
+        await msg.answer("Crocodillo Bombordiro")
 
